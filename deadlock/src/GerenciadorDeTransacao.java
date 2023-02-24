b98d7a6dc46c326d471d7fb8859331159fbb2cd6
Roger Pimenta <roger.pimenta@live.com>
2023-02-24 18:13:39 -0300

    entendendo deadlock

---
+++ deadlock/src/GerenciadorDeTransacao.java
@@ -0,0 +1,12 @@
+public class GerenciadorDeTransacao {
+    public void begin() {
+
+        System.out.println("Começando a transação");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ deadlock/src/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+        GerenciadorDeTransacao tx = new GerenciadorDeTransacao();
+        PoolDeConexao pool = new PoolDeConexao();
+
+        new Thread(new TarefaAcessaBanco(pool, tx)).start();
+        new Thread(new TarefaAcessaBancoProcedimento(pool, tx)).start();
+    }
+}
